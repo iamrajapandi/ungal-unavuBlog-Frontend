04322f51476d54db8461a3d43ee7bc11fd969469
Rajapandi <rrajapandi456@gmail.com>
2024-10-10 11:00:24 +0530

    first

---
+++ Blog-Frontend/src/services/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const SERVER_API = "https://ungal-unavublog-backend.onrender.com";
+
+
+// const getrecipes = () => axios.get(`${MOCK_API}/recipe`);
+const getrecipes = () => axios.get(`${SERVER_API}recipe/list`);
+const getRecipeId=()=>axios.get(`${SERVER_API}/recipe${id}`)
+const addRecipe=(Recipedata)=>axios.post(`${SERVER_API}recipe/add`,Recipedata)
+const editRecipe=(id,Recipedata)=>axios.put(`${SERVER_API}recipe/edit/${id}`,Recipedata)
+const deleteRecipe=(id)=>axios.delete(`${SERVER_API}recipe/delete/${id}`)
+export { getrecipes,getRecipeId,addRecipe,editRecipe,deleteRecipe };
